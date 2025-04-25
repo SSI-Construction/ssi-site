10332e13c4b825708dc3c1ae6339d93121cbe60f
SSI-Construction <info@ssiconstruction.ca>
2025-04-25 15:43:09 -0600

    security update

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,19 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("admin");
-    eleventyConfig.addPassthroughCopy("ssi_site_v2/images");
-  
-    return {
-      dir: {
-        input: "ssi_site_v2",
-        includes: "_includes",
-        output: "_site"
-      },
-      markdownTemplateEngine: "njk",
-      htmlTemplateEngine: "njk",
-      dataTemplateEngine: "njk"
-    };
+  eleventyConfig.addPassthroughCopy("ssi_site_v2/images");
+  eleventyConfig.addPassthroughCopy("ssi_site_v2/admin");
+
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("ssi_site_v2/posts/*.md");
+  });
+
+  return {
+    dir: {
+      input: "ssi_site_v2",
+      includes: "_includes",
+      output: "_site"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
   };
+};
