54ac029d4d3aac2b78f0b0a3a1eb983b185ba15a
SSI-Construction <info@ssiconstruction.ca>
2025-04-26 09:04:11 -0600

    date filter update

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,18 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+  // Add Luxon date filter
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  // Your existing passthrough copies
   eleventyConfig.addPassthroughCopy("ssi_site_v2/images");
   eleventyConfig.addPassthroughCopy("ssi_site_v2/admin");
+  eleventyConfig.addPassthroughCopy("ssi_site_v2/icon_310x310.png");
+  eleventyConfig.addPassthroughCopy("ssi_site_v2/ssi_walkthrough.mp4");
 
-  // THIS PART IS CRITICAL:
+  // Your existing collections
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("ssi_site_v2/posts/*.md");
   });
