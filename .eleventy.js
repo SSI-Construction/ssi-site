c78045496efe52b5cb9cd6121f86695162f4752b
kando <kando@example.com>
2026-04-26 15:38:13 -0600

    Pass through trade app overview video in eleventy build

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("ssi_site_v2/sign-up.mp4");
   eleventyConfig.addPassthroughCopy("ssi_site_v2/setting-tab.mp4");
   eleventyConfig.addPassthroughCopy("ssi_site_v2/ssi-construction-manager-overview.mp4");
+  eleventyConfig.addPassthroughCopy("ssi_site_v2/ssi-trade-app-overview.mp4");
 
   // Your existing collections
   eleventyConfig.addCollection("posts", function(collectionApi) {
